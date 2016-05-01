54651773ca151e9f34b62cd57fa4d9ee7eba0ae4
dwang68 <dalinwang68@gmail.com>
2016-04-30 20:29:33 -0700

    Changed the mongoDB URI to the one hosted by Heroku mongolab

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/scrum_test');
+mongoose.connect('mongodb://mongodb://heroku_c40snst4:fajnliljeaohnlvsg5692k782f@ds023478.mlab.com:23478/heroku_c40snst4');
 
 module.exports = mongoose.connection;
