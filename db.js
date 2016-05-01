db1fe78941b23468d0977550cbbb33f52f275622
dwang68 <dalinwang68@gmail.com>
2016-04-30 21:05:47 -0700

    Set the secure port to be different from the insecure one, also used the config var over string for MONGODB_URI

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_c40snst4:fajnliljeaohnlvsg5692k782f@ds023478.mlab.com:23478/heroku_c40snst4');
+mongoose.connect(process.env.MONGODB_URI);
 //mongoose.connect('mongodb://localhost:27017/scrum_test')
 
 module.exports = mongoose.connection;
