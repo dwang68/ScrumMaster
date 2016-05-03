e0207b66d35a861fe5743be5e1962424b5616d19
dwang68 <dalinwang68@gmail.com>
2016-05-03 13:13:29 -0700

    Fixed the illegal character

--- jira/index.js
+++ jira/index.js
@@ -25,7 +25,7 @@ var search = function(sessionID, jql_param, sprint, callback) {
 	            },
 	            data: {
 						// Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
-	                    jql: jql_param，
+	                    jql: jql_param,
 	                    "maxResults": 20
 	            	    //jql: 'type=Story AND status=Open AND Team=DEVX-Dev-Content AND "Story Points"=null AND project="Developer Experience Backlog" AND Sprint=null'
 
